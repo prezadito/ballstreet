bcdd52853a5ae18b15263a5bc76bef20d9ac613e
prezadito <aditya4mvp@gmail.com>
2016-07-20 01:01:03 -0400

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/codeGawd/pen/qNXryb.
+
+ A splash page for an up and coming startup. 
---
+++ js/index.js
@@ -0,0 +1,6 @@
+// https://coolors.co/app/2b2d42-8d99ae-edf2f4-ef233c-d90429
+
+// font-family: 'Baloo Da', cursive;
+// font-family: 'Alegreya', serif;
+
+// add background-img behind main aka 'jumbotron'
