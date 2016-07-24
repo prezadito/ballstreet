33ed162bd0a6f166a24728289dcd38b67a05619a
prezadito <aditya4mvp@gmail.com>
2016-07-24 12:12:17 -0400

    reference links

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@
 // font-family: 'Baloo Da', cursive;
 // font-family: 'Alegreya', serif;
 
+// http://www.htmlgoodies.com/tutorials/buttons/article.php/3478871
+// https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Forms/My_first_HTML_form
+// https://www.formget.com/how-to-create-pop-up-contact-form-using-javascript/
+
 function div_show() {
   document.getElementById('abc').style.display = "block";
 }
