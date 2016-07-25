cde743f8d3e90f71363ba293b442dbc53987e308
Aditya Niloi <aditya4mvp@gmail.com>
2016-07-24 21:56:24 -0400

    cleaned up comments

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-// https://coolors.co/app/2b2d42-8d99ae-edf2f4-ef233c-d90429
-
-// font-family: 'Baloo Da', cursive;
-// font-family: 'Alegreya', serif;
-// font-family: 'Rakkas', cursive;
-
-// http://www.htmlgoodies.com/tutorials/buttons/article.php/3478871
-// https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Forms/My_first_HTML_form
-// https://www.formget.com/how-to-create-pop-up-contact-form-using-javascript/
-
 // Validating Empty Field
 function check_empty() {
 	if (document.getElementById('name').value == "" || document.getElementById('email').value == "") {
